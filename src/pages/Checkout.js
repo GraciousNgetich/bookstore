34a9be0e6b77749d1ba4150bb9e037e646d0107e
GraciousNgetich <graciousngetich@gmail.com>
2022-02-01 20:10:03 +0300

    first commit

---
+++ src/pages/Checkout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from "../components/CheckoutForm";
+
+const Checkout = () => {
+    const stripePromise = loadStripe('pk_test_51KOPHWH5KAbEUk7to1t55Wa2DyhsPx6wsLeBWSzSNymd1kJlmpra0SIMQjs0qnC7PJPuK0PocRQ0IIWwEuKtSnCd00u0bPLJbU');
+
+    return (
+        <section className="checkout-wrapper">
+            <AmplifyAuthenticator>
+                <Elements stripe={stripePromise}>
+                    <section>
+                        <h2>Time to Checkout?</h2>
+                        <CheckoutForm />
+                    </section>
+                </Elements>
+            </AmplifyAuthenticator>
+        </section>
+    )
+}
+
+export default Checkout
